02d7d9e198aa2c76d7e3e27a6ee707f79b8aa0fa
LeagoNkadimeng60 <leagonkadimeng60@gmail.com>
2026-06-10 15:10:27 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/outdooradventure2/MainActivity.kt
+++ app/src/main/java/com/example/outdooradventure2/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,15 +20,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val itemName=arrayOf("Tent","Marshmallows","Flashligh" )
+            val catergory=arrayOf("Shelter","Food","Safety")
+            var quantity=arrayOf(1,3,2)
             OutdoorAdventure2Theme {
-                
-
+                Column() {
+                    Button(
+                        onClick = {}
+                    ) {
+                        Text("Item Name")
+                    }
+                    Button(
+                        onClick = {}
+                        ) {
+                        Text("Item Catergory")
+                    }
 
 
                 }
             }
         }
     }
+
 
 
 
